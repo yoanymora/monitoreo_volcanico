bc057ede961026a90d31378934df9e005f950373
Yoany Stephany Mora Pérez <yoany.stephany@gmail.com>
2019-08-31 14:07:17 -0500

    arreglo error de escritura

--- lectura_lm35/lectura_lm35.ino
+++ lectura_lm35/lectura_lm35.ino
@@ -1,20 +1,20 @@
-//leer temperatura::LM35
+//Leer LM35:Sensor de temperatura
 
-#define LM35 A0   //pin conectado a LM35
+#define lm35PIN A0   //pin conectado a LM35
 float raw_temp = 0, temp = 0; //variables globales para leer adc y obtener temperatura
 
 void setup() {
   Serial.begin(9600);
   analogReference(INTERNAL);
-  pinMode(LM35, INPUT);
+  pinMode(lm35PIN, INPUT);
 }
 
 void loop() {
-  raw_temp = analogRead(LM35); //leer sensor de temperatura
-  temp = ((raw_temp) * (1.1) * (100.0)) / 1024.0;//conversion de adc
+  raw_temp = analogRead(lm35PIN); //leer sensor de temperatura
+  temp = ((raw_temp) * (500.0)) / 1023.0;//conversion de adc
   Serial.print("Lectura ADC: ");
   Serial.println(raw_temp);//imprimir en consola
   Serial.print("Temperatura: ");
   Serial.println(temp);//imprimir en consola
-  delay(4000);
+  delay(2000);
 }
